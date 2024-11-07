96f97d35b5bf1906fe85c8772d7e5e755f74b433
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-07 09:45:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -91,3 +91,8 @@ class Cactus(pygame.sprite.Sprite):
         self.y_pos = y_pos
         self.sprites = []
         for i in range(1, 7):
+            current_sprite = pygame.transform.scale(
+                pygame.image.load(f"assets/cacti/cactus{i}.png"), (100, 100))
+            self.sprites.append(current_sprite)
+        self.image = random.choice(self.sprites)
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
