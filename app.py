bfd5c13d8c54f667ecc52731e943777e51726504
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-07 09:46:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -100,3 +100,12 @@ class Cactus(pygame.sprite.Sprite):
     def update(self):
         self.x_pos -= game_speed
         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+
+class Ptero(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.x_pos = 1300
+        self.y_pos = random.choice([280, 295, 350])
+        self.sprites = []
+        self.sprites.append(
