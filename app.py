a8debf9a9f6a7ce55e4e6d10029778f1f5a8a3ea
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-07 09:42:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,17 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Dino Game")
 
 game_font = pygame.font.Font("assets/PressStart2P-Regular.ttf", 24)
+
+# Classes
+
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self, image, x_pos, y_pos):
+        super().__init__()
+        self.image = image
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self):
+        self.rect.x -= 1
