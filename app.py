c5b46e8e1bd1f9c54c1f0001e23b935b1df79bed
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-07 09:42:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# Autor: Jonathan Hernández
+# Fecha: 7 Noviembre 2024
+# Descripción: Código game-dino.
+# GitHub: https://github.com/Jona163
+import pygame
+import sys
+import random
 
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+pygame.display.set_caption("Dino Game")
+
+game_font = pygame.font.Font("assets/PressStart2P-Regular.ttf", 24)
